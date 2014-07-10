fb2539d33410b2019c2e696ade0596c438a55611
Alan deLevie <adelevie@gmail.com>
2014-07-10 19:19:19 -0400

    added tests for map

--- advanced/test/test-fold.js
+++ advanced/test/test-fold.js
@@ -1,16 +1,16 @@
 var assert = require("assert")
-var fold = require("../fold")
+var fold = require("../solutions/fold")
 
 describe('foldl', function(){
   describe('#indexOf()', function(){
     it('foldl should add up some numbers', function() {
-	list = [3,4,5]
-        assert.equal(fold.foldl(function plus(a,b) { return a+b; },list,0),12);
+	    list = [3,4,5]
+      assert.equal(fold.foldl(function plus(a,b) { return a+b; },list,0),12);
     })
     it('foldr should equal foldl for commutative functions', function() {
-	list = [3,4,5]
-        assert.equal(fold.foldr(function plus(a,b) { return a+b; },list,0),
-		     fold.foldl(function plus(a,b) { return a+b; },list,0));
+	    list = [3,4,5]
+      assert.equal(fold.foldr(function plus(a,b) { return a+b; },list,0),
+		  fold.foldl(function plus(a,b) { return a+b; },list,0));
     })
   })
 })
